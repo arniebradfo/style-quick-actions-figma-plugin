7cbd53193ec96d2c44f181d5778c31759e3eac30
James Bradford <j.bradford@pnnl.gov>
2023-02-26 15:53:59 -0800

    individual manifest menu commands rather than parameters

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,68 @@
+import { publishStyles } from './publishStyles';
 import { svgIconEffect, svgIconGrid, svgIconText } from './svgIcon';
 import { svgIconPaint } from './svgIconPaint';
 import { searchSuggestions } from './utils';
 
+enum Command {
+	Fill = 'fill',
+	Stroke = 'stroke',
+	Text = 'text',
+	Effect = 'effect',
+	Grid = 'grid',
+	ToggleStyle = 'toggle',
+	PublishStyle = 'publish',
+	DeleteStyle = 'delete',
+}
+type Key = Command;
+
+type _RunEvent = {
+	command: Command;
+	parameters: Record<Command, string>;
+};
+
 figma.on('run', (event: RunEvent) => {
 	const { selection } = figma.currentPage;
-	const {command, parameters } = event;
-	const { parameterOne, parameterTwo } = parameters as ParametersType;
+	const { command, parameters } = event as _RunEvent;
+	const styleId = parameters?.[command] || '';
 
-	const styleId = parameterTwo; // figma.getStyleById(parameterTwo)?.id;
 	console.log({
 		command,
 		parameters,
-		style: figma.getStyleById(parameterTwo),
+		style: figma.getStyleById(styleId),
 		selection,
 	});
 
-	switch (parameterOne) {
+	switch (command) {
 		case Command.Fill:
 			selection.forEach((node) => {
-				if ('fillStyleId' in node) node.fillStyleId = parameterTwo;
+				if ('fillStyleId' in node) node.fillStyleId = styleId;
 			});
 			break;
 		case Command.Stroke:
 			selection.forEach((node) => {
-				if ('strokeStyleId' in node) node.strokeStyleId = parameterTwo;
+				if ('strokeStyleId' in node) node.strokeStyleId = styleId;
 			});
 			break;
 		case Command.Text:
 			selection.forEach((node) => {
-				if ('textStyleId' in node) node.textStyleId = parameterTwo;
+				if ('textStyleId' in node) node.textStyleId = styleId;
 			});
 			break;
 		case Command.Effect:
 			selection.forEach((node) => {
-				if ('effectStyleId' in node) node.effectStyleId = parameterTwo;
+				if ('effectStyleId' in node) node.effectStyleId = styleId;
 			});
 			break;
 		case Command.Grid:
 			selection.forEach((node) => {
-				if ('gridStyleId' in node) node.gridStyleId = parameterTwo;
+				if ('gridStyleId' in node) node.gridStyleId = styleId;
 			});
 			break;
 		case Command.ToggleStyle:
 			figma.notify("Styles from 'Filename' are now available in this file");
 			break;
 		case Command.PublishStyle:
-			figma.notify(
-				"(Updated) Published styles as 'File Name'. To use these styles in another file, select the name in 'Manage Styles'."
-			);
+			publishStyles();
 			break;
 		case Command.DeleteStyle:
 			figma.notify('Deleted Style');
@@ -60,72 +75,36 @@ figma.on('run', (event: RunEvent) => {
 	figma.closePlugin();
 });
 
-enum Command {
-	Fill,
-	Stroke,
-	Text,
-	Effect,
-	Grid,
-	ToggleStyle,
-	PublishStyle,
-	DeleteStyle,
-}
-type CommandSuggestion = SuggestionObj<Command>;
-type Key = 'parameterOne' | 'parameterTwo';
-type ParametersType = {
-	parameterOne: Command;
-	parameterTwo: string;
-};
-
-const styleTypeCommands: CommandSuggestion[] = [
-	{ name: `Fill`, data: Command.Fill },
-	{ name: `Stroke`, data: Command.Stroke },
-	{ name: `Typography`, data: Command.Text },
-	{ name: `Effect`, data: Command.Effect },
-	{ name: `Grid`, data: Command.Grid },
-];
-const manageStyleCommand = 'Manage Styles';
-const manageStyleOptions: CommandSuggestion[] = [
-	{ name: `${manageStyleCommand}: Toggle available published styles`, data: Command.ToggleStyle },
-	{ name: `${manageStyleCommand}: Publish current file's styles`, data: Command.PublishStyle },
-	{ name: `${manageStyleCommand}: Delete a published style`, data: Command.DeleteStyle },
-];
-const initialSuggestions = styleTypeCommands.concat(manageStyleOptions);
-
-figma.parameters.on('input', ({ parameters, key: _key, query, result }: ParameterInputEvent<ParametersType>) => {
+figma.parameters.on('input', ({ parameters, key: _key, query, result }: ParameterInputEvent) => {
 	const key = _key as Key;
 	console.log({ parameters, key, query, result });
-
-	if (key === 'parameterOne') {
-		result.setSuggestions(searchSuggestions(query, initialSuggestions));
-	} else if (key === 'parameterTwo') {
-		switch (parameters.parameterOne) {
-			case Command.Fill:
-				setPaintSuggestions(result, query);
-				break;
-			case Command.Stroke:
-				setPaintSuggestions(result, query);
-				break;
-			case Command.Text:
-				setTextSuggestions(result, query);
-				break;
-			case Command.Effect:
-				setEffectSuggestions(result, query);
-				break;
-			case Command.Grid:
-				setGridSuggestions(result, query);
-				break;
-			case Command.ToggleStyle:
-				break;
-			case Command.PublishStyle:
-				break;
-			case Command.DeleteStyle:
-				break;
-			default:
-				break;
-		}
+	switch (key) {
+		case Command.Fill:
+			setPaintSuggestions(result, query);
+			break;
+		case Command.Stroke:
+			setPaintSuggestions(result, query);
+			break;
+		case Command.Text:
+			setTextSuggestions(result, query);
+			break;
+		case Command.Effect:
+			setEffectSuggestions(result, query);
+			break;
+		case Command.Grid:
+			setGridSuggestions(result, query);
+			break;
+		case Command.ToggleStyle:
+			break;
+		case Command.PublishStyle:
+			break;
+		case Command.DeleteStyle:
+			break;
+		default:
+			break;
 	}
 });
+
 function setPaintSuggestions(result: SuggestionResults, query?: string) {
 	const paintStyles = figma.getLocalPaintStyles().map((style) => ({
 		name: style.name,
