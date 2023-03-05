139d56faf8e9591a558de34244099111cfb1b803
James Bradford <j.bradford@pnnl.gov>
2023-03-04 18:07:48 -0800

    memoize allPaintStyleSuggestions

--- src/onInput.ts
+++ src/onInput.ts
@@ -1,14 +1,14 @@
 import { getLibraryPaintStyles, setLibrarySuggestions } from './manageStyles';
-import { mapPaintStyleToStorage } from './mapStyle';
+import { mapPaintStyleToStorage, StoragePaintStyle } from './mapStyle';
 import { svgIconEffect, svgIconGrid, svgIconText } from './svgIcon';
 import { svgIconPaint } from './svgIconPaint';
 import { InputCommand, InputKey } from './types';
-import { searchSuggestions } from './utils';
+import { searchSuggestions, Suggestion } from './utils';
 
 export const onInput = async ({ parameters, key: _key, query, result }: ParameterInputEvent) => {
 	const key = _key as InputKey;
 	console.log({ parameters, key, query, result });
-	result.setLoadingMessage('Loading Options')
+	result.setLoadingMessage('Loading Options'); // doesn't run
 	switch (key) {
 		case InputCommand.Fill:
 			await setPaintSuggestions(result, query);
@@ -38,17 +38,19 @@ export const onInput = async ({ parameters, key: _key, query, result }: Paramete
 	}
 };
 
+let allPaintStyleSuggestions: Suggestion[] | null = null;
 async function setPaintSuggestions(result: SuggestionResults, query?: string) {
-	const localPaintStyles = figma.getLocalPaintStyles().map(mapPaintStyleToStorage);
-	const remotePaintStyles = await getLibraryPaintStyles()
-	const paintStyleSuggestions = localPaintStyles
-		.concat(remotePaintStyles)
-		.map((style) => ({
+	if (allPaintStyleSuggestions == null) {
+		// memoize allPaintStyleSuggestions
+		const localPaintStyles = figma.getLocalPaintStyles().map(mapPaintStyleToStorage);
+		const remotePaintStyles = await getLibraryPaintStyles();
+		allPaintStyleSuggestions = localPaintStyles.concat(remotePaintStyles).map((style) => ({
 			data: style[0],
 			name: style[1],
 			icon: svgIconPaint(style),
 		}));
-	result.setSuggestions(searchSuggestions(query, paintStyleSuggestions));
+	}
+	result.setSuggestions(searchSuggestions(query, allPaintStyleSuggestions));
 }
 
 function setTextSuggestions(result: SuggestionResults, query?: string) {
