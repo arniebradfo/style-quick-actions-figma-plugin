d58e871b45051e2430d73779bd5263b6ac614d5e
James Bradford <j.bradford@pnnl.gov>
2023-04-09 12:13:44 -0700

    add style to selected group children

--- src/onRun.ts
+++ src/onRun.ts
@@ -3,7 +3,6 @@ import { SuggestionData } from './suggestions/suggestion';
 import { figmaNotifyErrorOptions, InputCommand } from './utils';
 
 export async function onRun(event: RunEvent) {
-	const { selection } = figma.currentPage;
 	const { command, parameters } = event as _RunEvent;
 	const parameterData = parameters?.[command];
 
@@ -25,11 +24,15 @@ export async function onRun(event: RunEvent) {
 				break;
 		}
 	} else {
+		const { selection } = figma.currentPage;
+
 		if (selection.length === 0) {
 			figma.notify('No Layers Selected');
 			figma.closePlugin();
 			return;
 		}
+
+		const nodes = flattenGroupChildren(selection);
 
 		const { id: styleIdOrKey, source } = (parameterData as SuggestionData) ?? {};
 
@@ -58,27 +61,27 @@ export async function onRun(event: RunEvent) {
 
 		switch (command) {
 			case InputCommand.Fill:
-				selection.forEach((node) => {
+				nodes.forEach((node) => {
 					if ('fillStyleId' in node) node.fillStyleId = styleId;
 				});
 				break;
 			case InputCommand.Stroke:
-				selection.forEach((node) => {
+				nodes.forEach((node) => {
 					if ('strokeStyleId' in node) node.strokeStyleId = styleId;
 				});
 				break;
 			case InputCommand.Text:
-				selection.forEach((node) => {
+				nodes.forEach((node) => {
 					if ('textStyleId' in node) node.textStyleId = styleId;
 				});
 				break;
 			case InputCommand.Effect:
-				selection.forEach((node) => {
+				nodes.forEach((node) => {
 					if ('effectStyleId' in node) node.effectStyleId = styleId;
 				});
 				break;
 			case InputCommand.Grid:
-				selection.forEach((node) => {
+				nodes.forEach((node) => {
 					if ('gridStyleId' in node) node.gridStyleId = styleId;
 				});
 				break;
@@ -95,3 +98,7 @@ type _RunEvent = {
 	command: InputCommand;
 	parameters: Record<InputCommand, SuggestionData | string>;
 };
+
+function flattenGroupChildren(nodes: readonly SceneNode[]): SceneNode[] {
+	return nodes.flatMap((node) => (node.type === 'GROUP' ? flattenGroupChildren(node.children) : node));
+}
